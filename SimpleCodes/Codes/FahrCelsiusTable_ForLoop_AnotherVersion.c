35343b2265d2f9b88d036f0ac5d764834ba996ff
rosaj12 <nemomiikka12@gmail.com>
2025-08-07 05:37:41 +0000

    Change 01

--- SimpleCodes/Codes/FahrCelsiusTable_ForLoop_AnotherVersion.c
+++ SimpleCodes/Codes/FahrCelsiusTable_ForLoop_AnotherVersion.c
@@ -3,19 +3,19 @@
 
 #define lower 0
 #define upper 300
-#define step 10
+#define step 5
 
 /* print Fahrenheit-Celisus table
-	for fahr = 0, 10, ..., 300; floating-poit version*/
+	for fahr = 0, 5, ..., 300; floating-poit version*/
 	
 int main()
 {
 	float fahr, celsius;	
 
-	for(fahr = lower; fahr <= upper; fahr = fahr + step) {
+	for(fahr = lower; fahr <= upper; fahr += step) {
 		celsius = (5.0/9.0) * (fahr - 32.0);
 		
-		printf("%3.0f\t %6.1f\n", fahr, celsius);
+		printf("%.1f\t %.1f\n", fahr, celsius);
 	}
 			
 	return 0;
